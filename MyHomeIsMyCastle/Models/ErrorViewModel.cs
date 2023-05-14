5b5c68e680ff145c8af80ef830125d68f9b2e68d
That_Abyss_Egg <probablynotfaputa@gmail.com>
2023-05-14 20:15:35 +0300

    Add project files.

---
+++ MyHomeIsMyCastle/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyHomeIsMyCastle.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
